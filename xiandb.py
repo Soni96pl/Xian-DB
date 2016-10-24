e2249cafb15bbf75c29ea600870d42e5452509aa
Jakub Chronowski <jakub@chronow.ski>
2016-10-24 15:06:45 +0200

    Fix PEP8 E501 warning in xiandb.py

--- xiandb.py
+++ xiandb.py
@@ -6,8 +6,8 @@ import yaml
 from datetime import datetime
 
 try:
-    with open(os.path.expanduser('~') + '/xian/config.yml', 'r') as config_file:
-        cfg = yaml.load(config_file)
+    with open(os.path.expanduser('~') + '/xian/config.yml', 'r') as cfg_file:
+        cfg = yaml.load(cfg_file)
 except IOError:
     print "Couldn't find a valid configuration file in ~/xian/config.xml"
     print "Please refer to README.rst"
