d3c2b92b4f93ff0180ad9d88e62ea9010d255581
Jakub Chronowski <jakub@chronow.ski>
2016-11-10 11:25:22 +0700

    Remove unused database variable

--- xiandb/__init__.py
+++ xiandb/__init__.py
@@ -4,9 +4,6 @@ from xiandb import config
 from xiandb.models import city, user, trip
 
 client = MongoClient(host=config.mongodb['host'], port=config.mongodb['port'])
-database = config.mongodb['database']
-
-
 City = city.CityCollection(client=client)
 User = user.UserCollection(client=client)
 Trip = trip.TripCollection(client=client)
