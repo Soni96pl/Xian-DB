4f7c6982a2d05d49eb38f69afe1577c222d52b66
Jakub Chronowski <jakub@chronow.ski>
2016-11-15 14:25:22 +0700

    Fix missing imports.

--- xiandb/__init__.py
+++ xiandb/__init__.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
 from xiandb import config
-from xiandb.models import city, user, trip
+from xiandb.models import city, user, trip, carrier, station
 
 client = MongoClient(host=config.mongodb['host'], port=config.mongodb['port'])
 City = city.CityCollection(client=client)
 User = user.UserCollection(client=client)
 Trip = trip.TripCollection(client=client)
-Carrier = trip.CarrierCollection(client=client)
-Station = trip.StationCollection(client=client)
+Carrier = carrier.CarrierCollection(client=client)
+Station = station.StationCollection(client=client)
